01f2ead01c9ea9fc8dc8a1122d80200dbe26e2e1
Emily Wagner <emilywagner598@gmail.com>
2019-02-01 01:14:26 +0000

    Automatically backed up by Learn

--- lib/alphabetize.rb
+++ lib/alphabetize.rb
@@ -2,5 +2,5 @@ def alphabetize(arr)
   esp_alph = " abcĉdefgĝhĥijĵklmnoprsŝtuŭvz"
   ascii = "@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\"
 
-  arr.sort_by { |word| word.tr(esp_alph, ascii)[0] }
+  arr.sort_by { |word| word.tr(esp_alph, ascii)[1] }
 end
